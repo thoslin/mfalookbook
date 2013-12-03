d7e05d1665bbce4013e69b701775febf8ea25f54
Tom Lin <lintao.im@gmail.com>
2013-12-03 11:13:33 +0800

    Load thumbnails instead of original image

--- mfa/mfa/lookbook/app.py
+++ mfa/mfa/lookbook/app.py
@@ -1,18 +1,31 @@
 from flask import Flask, render_template
+from jinja2 import Environment, PackageLoader
 from ..models import Session, Comment
 
 
 app = Flask(__name__)
+env = Environment(loader=PackageLoader('mfa.lookbook', 'templates'))
+
+
+def thumbnail(url, size="m"):
+    """ Get thumbnail for imgur.com image """
+    parts = url.rsplit(".", 1)
+    if len(parts) == 2:
+        _size = "%s." % size
+        return _size.join(parts)
+    return url
+env.filters["thumbnail"] = thumbnail
 
 
 @app.route("/")
 def index():
     session = Session()
-    comments = session.query(Comment).all()[:15]
+    comments = session.query(Comment).order_by("-point")[:15]
     session.close()
-    return render_template("index.html", comments=comments)
+    return render_template(env.get_template("index.html"),
+                           comments=comments)
 
 
 @app.route("/archive")
 def archive():
-    return render_template("archive.html")
+    return render_template(env.get_template("archive.html"))
--- mfa/runapp.py
+++ mfa/runapp.py
@@ -1,3 +1,5 @@
 if __name__ == "__main__":
     from mfa.lookbook.app import app
+    from mfa.models import init_db
+    init_db()
     app.run(debug=True)
